2baaa9ea6d5e780fb84bfa3edbe36aa44ea54aef
Weerawut Chaiyasomboon <weerawut@live.com>
2023-12-22 10:51:16 +0700

    Created MeetingView.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 10, total: 15)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            
+            Spacer()
+            
+            Circle()
+                .stroke(lineWidth: 24)
+            
+            Spacer()
+            
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}, label: {
+                    Image(systemName: "forward.fill")
+                })
+                .accessibilityLabel("Next Speaker")
+            }
+        }
+        .padding()
     }
 }
 
