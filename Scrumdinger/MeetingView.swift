2f9904025ae7b35dd465b977d75ee2e93d1ba664
Weerawut Chaiyasomboon <weerawut@live.com>
2023-12-22 10:13:59 +0700

    Initial Project.

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Weerawut Chaiyasomboon on 22/12/2566 BE.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        ProgressView(value: 10, total: 15)
+    }
+}
+
+#Preview {
+    MeetingView()
+}
